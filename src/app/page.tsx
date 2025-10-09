490883d2595ab09b5aa5d24f625b5fdf9c74b635
paul-rl <35239075+paul-rl@users.noreply.github.com>
2025-10-09 19:15:39 -0400

    Added carousel to page! :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from "next/image";
+// page.tsx
 import HeaderTabs from "./components/HeaderTabs";
+import ItemCard from "./components/ItemCard";
+import ProjectsCarousel from "./components/ProjectsCarousel";
 
 export default function Home() {
   return (
-    <HeaderTabs className = "py-6 justify-center"/>
+    <div className="min-h-screen  flex flex-col items-center px-4 sm:px-8 font-sans text-cream">
+      <HeaderTabs className="py-6 justify-center" />
+      <ItemCard className="mt-12" />
+      <ProjectsCarousel className="w-full max-w-[860px] mx-auto" />
+    </div>
   );
 }
