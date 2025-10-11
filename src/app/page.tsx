9928227906dad978572bf26fb8431d43e2c9d058
paul-rl <35239075+paul-rl@users.noreply.github.com>
2025-10-11 15:54:24 -0400

    Fixed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HeaderTabs from "./components/HeaderTabs";
 import ItemShowcase from "./components/ItemShowcase";
-import ProjectsCarousel from "./components/Carousels/ProjectsCarousel";
+import ProjectsCarousel from "./components/carousels/ProjectsCarousel";
 
 const slides = [
   "/images/projects/keystone.jpg",
@@ -14,14 +14,13 @@ export default function Home() {
     <div className="min-h-screen flex flex-col items-center w-full px-[clamp(1rem,2vw,3rem)] mx-auto">
       <HeaderTabs className="py-6 justify-center" />
       <ItemShowcase
-  title="Project Title"
-  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo risus, semper sed nulla eu, interdum suscipit sapien. Proin maximus vel orci ac euismod. Quisque nec lacus ullamcorper, cursus orci a, ullamcorper mauris. In non interdum neque. Phasellus blandit mauris lacus, nec maximus tellus pretium vitae. Praesent urna lacus, interdum non ultrices at, porta scelerisque ligula. Sed eros velit, vulputate non pretium ac, gravida at lorem. In posuere in ex non condimentum."
-  imageSrc={slides[3]}
-  tags={["Unity", "Java"]}
-  textMaxWidth="69ch"
-  className="mb-3"
-/>
-
+        title="Project Title"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo risus, semper sed nulla eu, interdum suscipit sapien. Proin maximus vel orci ac euismod. Quisque nec lacus ullamcorper, cursus orci a, ullamcorper mauris. In non interdum neque. Phasellus blandit mauris lacus, nec maximus tellus pretium vitae. Praesent urna lacus, interdum non ultrices at, porta scelerisque ligula. Sed eros velit, vulputate non pretium ac, gravida at lorem. In posuere in ex non condimentum."
+        imageSrc={slides[0]}
+        tags={["Unity", "Java"]}
+        textMaxWidth="69ch"
+        className="mb-3"
+      />
       <ProjectsCarousel />
     </div>
   );
