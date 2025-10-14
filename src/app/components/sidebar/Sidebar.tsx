dfceeeb6ba6d657a7a9433f527f148ca6e9f278c
paul-rl <35239075+paul-rl@users.noreply.github.com>
2025-10-14 03:21:20 -0400

    Updated code to include about me section in header

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,14 +1,18 @@
+"use client"
+
+import { useSiteData } from "@/app/components/SiteDataContext";
 import SidebarHeader from "@/app/components/sidebar/SidebarHeader";
 import InfoSection from "@/app/components/sidebar/InfoSection";
 import ButtonCollection from "@/app/components/sidebar/ButtonCollection";
 
 export default function Sidebar() {
+  const data = useSiteData();
   return (
     <div className="p-6 space-y-6">
-      <SidebarHeader className="mb-[4rem]" />
-      <InfoSection className="mb-[4rem]" />
-      <p className="mb-[4rem] leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo risus, semper sed nulla eu, interdum suscipit sapien. Proin maximus vel orci ac euismod. Quisque nec lacus ullamcorper, cursus orci a, ullamcorper mauris. In non interdum neque. Phasellus blandit mauris lacus, nec maximus tellus pretium vitae. Praesent urna lacus, interdum non ultrices at, porta scelerisque ligula. Sed eros velit, vulputate non pretium ac, gravida at lorem. In posuere in ex non condimentum.
+      <SidebarHeader className="mb-[2rem]" />
+      <InfoSection className="mb-[2rem]" />
+      <p className="mb-[2rem] leading-relaxed">
+        {data.aboutMe}
       </p>
       <ButtonCollection className="space-y-4" />
     </div>
